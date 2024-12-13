82da1c494e016be9c4e0a41c3d56894e7ac97410
gwirn <71886945+gwirn@users.noreply.github.com>
2024-12-13 10:46:43 +0100

    removed flag

--- loc_production_server/clean.sh
+++ loc_production_server/clean.sh
@@ -9,8 +9,8 @@ server_path=$(grep loc_prod_path "$path_path" | sed 's/.*://')
 date +%Y-%m-%d-%H-%M-%S
 
 for i in $(find "${storage_path}" -mindepth 1 -type d -mtime 7 -not -name ".shutdown" -and -not -name ".backup" | awk ' BEGIN { FS = "/" } length($6) > 1  { print $5"/"$6 }' | sort | uniq); do
-    rm -r -ltr "$storage_path/${i}"
-    rm -ltr "$storage_path/${i}.zip"
+    rm -r "$storage_path/${i}"
+    rm "$storage_path/${i}.zip"
 done
  find "${server_path}/schedule/exe_scripts/" -mindepth 1  -type f -mtime +7 -delete
 
