564253dc3ea662ba40fa08a1acbb099314e7b770
gwirn <71886945+gwirn@users.noreply.github.com>
2025-03-10 11:26:38 +0100

    remove old user directories

--- loc_production_server/clean.sh
+++ loc_production_server/clean.sh
@@ -8,12 +8,20 @@ server_path=$(grep loc_prod_path "$path_path" | sed 's/.*://')
 
 date +%Y-%m-%d-%H-%M-%S
 
+# deletes results of run jobs
 for i in $(find "${storage_path}" -mindepth 1 -type d -mtime 7 -not -name ".shutdown" -and -not -name ".backup" | awk ' BEGIN { FS = "/" } length($6) > 1  { print $5"/"$6 }' | sort | uniq); do
     rm -r "$storage_path/${i}"
     rm "$storage_path/${i}.zip"
 done
- find "${server_path}/schedule/exe_scripts/" -mindepth 1  -type f -mtime +7 -delete
+
+# deletes the execution scripts for jobs 
+find "${server_path}/schedule/exe_scripts/" -mindepth 1  -type f -mtime +7 -delete
+
+# deletes user folders 
+find "$storage_path" -mindepth 1 -type d -not -name ".shutdown" -not -name ".backup" -mtime +7 -delete
 
 if [ ! -d "$storage_path" ];then
     mkdir "$storage_path"
+    mkdir "$storage_path/.shutdown"
+    mkdir "$storage_path/.backup"
 fi
